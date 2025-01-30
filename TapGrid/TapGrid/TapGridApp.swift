c55aa9aa658987dc6adf56d2a1eece13fc616149
Ming Xia <githubming@hotmail.com>
2025-01-30 03:37:53 -0800

    update files

---
+++ TapGrid/TapGrid/TapGridApp.swift
@@ -0,0 +1,17 @@
+//
+//  NumberGridApp.swift
+//  NumberGrid
+//
+//  Created by Ming Xia on 3/3/22.
+//
+
+import SwiftUI
+
+@main
+struct NumberGridApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

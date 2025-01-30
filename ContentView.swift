8b0af906071d07a089c7b542fda32c1b2d0833f0
Ming Xia <githubming@hotmail.com>
2025-01-30 03:31:10 -0800

    add files

---
+++ ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  NumberGrid
+//
+//  Created by Ming Xia on 3/3/22.
+//
+
+import SwiftUI
+
+
+struct ContentView: View {
+    
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
+    
